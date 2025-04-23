e7f8589ea75166f9a33c2c66dc13d644997a26f0
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-23 11:27:16 +0900

    Initial Commit

---
+++ CleanEx/CleanExApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanExApp.swift
+//  CleanEx
+//
+//  Created by 최범수 on 2025-04-23.
+//
+
+import SwiftUI
+
+@main
+struct CleanExApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
