5986a403cdf631aa93579554b71b72e59e2e5e7d
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-27 21:11:57 +0900

    TodoRepositoryImpl 구현.

--- CleanEx/Data/Repositories/TodoRepositoryImpl.swift
+++ CleanEx/Data/Repositories/TodoRepositoryImpl.swift
@@ -1,1 +1,35 @@
 import Foundation
+
+struct TodoRepositoryImpl: TodoRepository {
+    private let dataSource: TodoLocalDataSource
+    
+    init(dataSource: TodoLocalDataSource) {
+        self.dataSource = dataSource
+    }
+    
+    func getAllTodos() async throws -> [Todo] {
+        let dtos = try await dataSource.getAllTodos()
+        return dtos.map { $0.toDomain() }
+    }
+    
+    func getTodo(id: UUID) async throws -> Todo {
+        let dto = try await dataSource.getTodo(id: id)
+        return dto.toDomain()
+    }
+    
+    func addTodo(todo: Todo) async throws -> Todo {
+        let dto = TodoDTO.fromEntity(todo)
+        let createdDto = try await dataSource.addTodo(dto)
+        return createdDto.toDomain()
+    }
+    
+    func updateTodo(todo: Todo) async throws -> Todo {
+        let dto = TodoDTO.fromEntity(todo)
+        let updatedTodo = try await dataSource.updateTodo(dto)
+        return updatedTodo.toDomain()
+    }
+    
+    func deleteTodo(id: UUID) async throws {
+        try await dataSource.deleteTodo(id: id)
+    }
+}
