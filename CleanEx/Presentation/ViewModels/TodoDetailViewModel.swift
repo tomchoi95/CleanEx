ac963df768edccffb8df9bf0aff00150ef2be1ee
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-27 21:12:24 +0900

    뷰모델 구현.

--- CleanEx/Presentation/ViewModels/TodoDetailViewModel.swift
+++ CleanEx/Presentation/ViewModels/TodoDetailViewModel.swift
@@ -1,8 +1,51 @@
-//
-//  TodoDetailViewModel.swift
-//  CleanEx
-//
-//  Created by 최범수 on 2025-04-27.
-//
-
 import Foundation
+
+@MainActor
+final class TodoDetailViewModel: ObservableObject {
+    private let getTodoUseCase: GetTodoUseCase
+    private let updateTodoUseCase: UpdateTodoUseCase
+    
+    @Published private(set) var todo: Todo?
+    @Published private(set) var error: TodoViewError?
+    @Published private(set) var isLoading = false
+    
+    init(
+        getTodoUseCase: GetTodoUseCase,
+        updateTodoUseCase: UpdateTodoUseCase
+    ) {
+        self.getTodoUseCase = getTodoUseCase
+        self.updateTodoUseCase = updateTodoUseCase
+    }
+    
+    func loadTodo(id: UUID) {
+        Task {
+            isLoading = true
+            error = nil
+            
+            do {
+                todo = try await getTodoUseCase.execute(id: id)
+            } catch {
+                self.error = .failedToLoad
+            }
+            
+            isLoading = false
+        }
+    }
+    
+    func updateTodo(title: String, description: String?, isCompleted: Bool) {
+        guard let todo = todo else { return }
+        
+        Task {
+            do {
+                self.todo = try await updateTodoUseCase.execute(
+                    id: todo.id,
+                    title: title,
+                    description: description,
+                    isCompleted: isCompleted
+                )
+            } catch {
+                self.error = .failedToUpdate
+            }
+        }
+    }
+}
--- CleanEx/Presentation/ViewModels/TodoListViewModel.swift
+++ CleanEx/Presentation/ViewModels/TodoListViewModel.swift
@@ -1,8 +1,45 @@
-//
-//  TodoListViewModel.swift
-//  CleanEx
-//
-//  Created by 최범수 on 2025-04-27.
-//
-
 import Foundation
+
+@MainActor
+final class TodoListViewModel: ObservableObject {
+    private let getAllTodosUseCase: GetAllTodosUseCase
+    private let deleteTodoUseCase: DeleteTodoUseCase
+    
+    @Published private(set) var todos: [Todo] = []
+    @Published private(set) var error: TodoViewError?
+    @Published private(set) var isLoading = false
+    
+    init(
+        getAllTodosUseCase: GetAllTodosUseCase,
+        deleteTodoUseCase: DeleteTodoUseCase
+    ) {
+        self.getAllTodosUseCase = getAllTodosUseCase
+        self.deleteTodoUseCase = deleteTodoUseCase
+    }
+    
+    func loadTodos() {
+        Task {
+            isLoading = true
+            error = nil
+            
+            do {
+                todos = try await getAllTodosUseCase.execute()
+            } catch {
+                self.error = .failedToLoad
+            }
+            
+            isLoading = false
+        }
+    }
+    
+    func deleteTodo(id: UUID) {
+        Task {
+            do {
+                try await deleteTodoUseCase.execute(id: id)
+                loadTodos()  // 목록 새로고침
+            } catch {
+                self.error = .failedToDelete
+            }
+        }
+    }
+}
