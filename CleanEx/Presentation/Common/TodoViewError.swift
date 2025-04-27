8ff546fa4ae8098aa21c687e42078f9e795d25c3
Tom Bum Su Choi <qjatn0545@naver.com>
2025-04-27 21:12:10 +0900

    에러 정의.

--- CleanEx/Presentation/Common/TodoViewError.swift
+++ CleanEx/Presentation/Common/TodoViewError.swift
@@ -1,8 +1,24 @@
-//
-//  TodoViewError.swift
-//  CleanEx
-//
-//  Created by 최범수 on 2025-04-27.
-//
-
 import Foundation
+
+enum TodoViewError: LocalizedError {
+    case failedToLoad
+    case failedToCreate
+    case failedToUpdate
+    case failedToDelete
+    case unknown
+    
+    var errorDescription: String? {
+        switch self {
+        case .failedToLoad:
+            return "할 일 목록을 불러오는데 실패했습니다."
+        case .failedToCreate:
+            return "할 일 생성에 실패했습니다."
+        case .failedToUpdate:
+            return "할 일 수정에 실패했습니다."
+        case .failedToDelete:
+            return "할 일 삭제에 실패했습니다."
+        case .unknown:
+            return "알 수 없는 오류가 발생했습니다."
+        }
+    }
+}
